dfb7377af68701d71629dd8215742e087b589070
regrenos <sk300@duke.edu>
2014-10-07 22:42:30 -0400

    refactored structural commands

--- src/commandParsing/structuralCommandParsing/If.java
+++ src/commandParsing/structuralCommandParsing/If.java
@@ -1,41 +1,34 @@
 package commandParsing.structuralCommandParsing;
 
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.Queue;
 
-import stateUpdate.IfElse;
-import stateUpdate.StateUpdate;
 import commandParsing.exceptions.CompileTimeParsingException;
 import commandParsing.exceptions.RunTimeDivideByZeroException;
 import commandParsing.exceptions.RunTimeNullPointerException;
+
 import drawableobject.DrawableObject;
 
 
-public class If extends StructuralCommand {
-
+public class If extends StructuralCommandOnBooleanSwitch {
+	
 	@Override
 	public float parse(Iterator<String> commandString, Queue<DrawableObject> objectQueue) throws CompileTimeParsingException, RunTimeDivideByZeroException, RunTimeNullPointerException {
-
+		
 		accumulateComponents(commandString,1,objectQueue);
-		float booleanSwitch = expressionComponents.get(0);
-		Queue<DrawableObject> ifTrue = new LinkedList<DrawableObject>();
+		evaluateBooleanExpression();
+		checkForOpeningBrace(commandString);
 		
-		String stringOfInterest = commandString.next();
-		float returnValue = 0;
-		
-		if(!stringOfInterest.equals("[")){
-			throw new CompileTimeParsingException("expected opening brace");
-		}
-		else if(booleanSwitch!=0){
-			returnValue = generateQueueBetweenBraces(commandString, ifTrue);
+		if(booleanSwitch){
+			extractCommandsBetweenBraces(commandString);
 		}
 		else {
 			ignoreUntilClosingBrace(commandString);
+			setEnclosedCommandsToEmptyList();
 		}
 		
-		objectQueue.addAll(ifTrue);
-
+		parseCommandsBetweenBraces(enclosedCommands, objectQueue);
+		
 		return returnValue;
 	}
 
