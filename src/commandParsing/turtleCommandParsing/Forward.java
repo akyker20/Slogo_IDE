9ae747e4e45cd65bbd8a721bd21c58e1966c4d29
Steve Kuznetsov <steve.kuznetsov@duke.edu>
2014-10-10 13:36:19 -0400

    fixed location bug in line drawer

--- src/commandParsing/turtleCommandParsing/Forward.java
+++ src/commandParsing/turtleCommandParsing/Forward.java
@@ -15,8 +15,8 @@ public class Forward extends OneInputFloatCommandParser implements LineGenerator
     @Override
 	protected double operateOnComponents(List<Double> components, Queue<DrawableObject> objectQueue) throws RunTimeDivideByZeroException{
 		double distanceToMove = expressionComponents.get(0);
-    	state.moveForward(distanceToMove);
         objectQueue.add(generateDrawableObjectRepresntingLine(distanceToMove, state));
+        state.moveForward(distanceToMove);
 		return distanceToMove;
 	}
 
