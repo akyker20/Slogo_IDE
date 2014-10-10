cadda1bd7263b7bfaa2ea2d475d23afbb727e0e4
regrenos <sk300@duke.edu>
2014-10-10 01:27:12 -0400

    made LineFactory work again

--- src/gui/factories/LineFactory.java
+++ src/gui/factories/LineFactory.java
@@ -20,13 +20,13 @@ public class LineFactory extends ObjectFactory {
     public Node generateObject (Map<String, String> params) {
 
         Line line = new Line();
-//        double[] origin = parseStringToPoints(params.get(TurtleMovementInterpreter.ORIGIN2D));
-//        double[] destination = parseStringToPoints(params.get(TurtleMovementInterpreter.DESTINATION2D));
-//        
-//        line.setStartX(origin[0]);
-//        line.setStartY(origin[1]);
-//        line.setEndX(destination[0]);
-//        line.setEndY(destination[1]);
+        double[] origin = parseStringToPoints(params.get(ORIGIN));
+        double[] destination = parseStringToPoints(params.get(DESTINATION));
+        
+        line.setStartX(origin[0]);
+        line.setStartY(origin[1]);
+        line.setEndX(destination[0]);
+        line.setEndY(destination[1]);
 
         return line;
     }
@@ -34,8 +34,8 @@ public class LineFactory extends ObjectFactory {
     private double[] parseStringToPoints(String point) {
         String[] splitPoint = point.split(" ");
         
-        double[] parsedPoint = new double[]{(double) Double.parseDouble(splitPoint[0]),
-                                            (double) Double.parseDouble(splitPoint[1])};
+        double[] parsedPoint = new double[]{Double.parseDouble(splitPoint[0]),
+                                            Double.parseDouble(splitPoint[1])};
         return parsedPoint;
     }
 
