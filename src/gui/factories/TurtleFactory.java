51ca5916e4b34b179e149652aec453c23e6ee87e
Allan Kiplagat <allan.kiplagat@duke.edu>
2014-10-10 23:23:01 -0400

    added multiple turtle functionality

--- src/gui/factories/TurtleFactory.java
+++ src/gui/factories/TurtleFactory.java
@@ -2,6 +2,7 @@ package gui.factories;
 
 import gui.componentdrawers.ComponentInitializer;
 import gui.componentdrawers.GridDrawer;
+import java.util.HashMap;
 import java.util.Map;
 import javafx.scene.Node;
 import javafx.scene.image.Image;
@@ -9,41 +10,60 @@ import javafx.scene.image.ImageView;
 
 
 public class TurtleFactory extends ObjectFactory {
-    
+
     public static final String PARENT = ComponentInitializer.GRID_DRAWER;
     public static final String TYPE = FactoryInitializer.TURTLE_FACTORY;
     public static final String HEADING = "heading";
     public static final String LOCATION = "location";
     public static final String OPACITY = "opacity";
-    
+
     private static final String DEFAULT_TURTLE_IMAGEPATH = "turtle_image.png";
     private static final double TURTLE_IMAGE_WIDTH_RATIO = 0.05;
     private static final double TURTLE_IMAGE_WIDTH = GridDrawer.GRID_WIDTH *
-                                                    TURTLE_IMAGE_WIDTH_RATIO;
+            TURTLE_IMAGE_WIDTH_RATIO;
     private static final double TURTLE_IMAGE_HEIGHT_RATIO = 0.1;
     private static final double TURTLE_IMAGE_HEIGHT = GridDrawer.GRID_HEIGHT *
-                                                     TURTLE_IMAGE_HEIGHT_RATIO;
+            TURTLE_IMAGE_HEIGHT_RATIO;
+
+    private static final String TURTLE_IMAGE_ID = "turtleImageID";
     
-    private Node myImageView;
+    private Map<String,Node> myTurtleViews;
 
     public TurtleFactory (String name) {
         super(name);
-       Image image =
-                new Image(getClass().getResourceAsStream(DEFAULT_TURTLE_IMAGEPATH),
-                          TURTLE_IMAGE_WIDTH, TURTLE_IMAGE_HEIGHT,
-                          false, true);
-        myImageView = new ImageView(image);
+        myTurtleViews = new HashMap<String,Node>();
+
     }
 
     @Override
     public Node generateObject (Map<String, String> params) {
-        double[] newLocation = parseStringToPoints(params.get(LOCATION));
-        myImageView.setLayoutX(newLocation[0] + GridDrawer.GRID_WIDTH/2 - TURTLE_IMAGE_WIDTH/2);
-        myImageView.setLayoutY(- newLocation[1] + GridDrawer.GRID_HEIGHT/2 - TURTLE_IMAGE_HEIGHT/2);
-        myImageView.setRotate(Double.parseDouble(params.get(HEADING)));
-        myImageView.setOpacity(Double.parseDouble(params.get(OPACITY)));
-        return myImageView;
+        String turtleID = params.get(TURTLE_IMAGE_ID);
+        if (myTurtleViews.containsKey(turtleID)) {
+            updateTurtleImage(params);
+        } else {
+            createTurtleImage(params);
+        }
+        return new NullNode();
     }
+
+    private void createTurtleImage(Map<String, String> params) {      
+        Image image =
+                new Image(getClass().getResourceAsStream(DEFAULT_TURTLE_IMAGEPATH),
+                          TURTLE_IMAGE_WIDTH, TURTLE_IMAGE_HEIGHT,
+                          false, true);
+        myTurtleViews.put(params.get(TURTLE_IMAGE_ID), new ImageView(image));
+        updateTurtleImage(params);
+    }
+
+    private void updateTurtleImage(Map<String, String> params) {
+        Node currentImageView = myTurtleViews.get(params.get(TURTLE_IMAGE_ID));
+        double[] newLocation = parseStringToPoints(params.get(LOCATION));
+        currentImageView.setLayoutX(newLocation[0] + GridDrawer.GRID_WIDTH/2 - TURTLE_IMAGE_WIDTH/2);
+        currentImageView.setLayoutY(- newLocation[1] + GridDrawer.GRID_HEIGHT/2 - TURTLE_IMAGE_HEIGHT/2);
+        currentImageView.setRotate(Double.parseDouble(params.get(HEADING)));
+        currentImageView.setOpacity(Double.parseDouble(params.get(OPACITY)));      
+    }
+
 
     private double[] parseStringToPoints(String point) {
         String[] splitPoint = point.split(" ");
