77001266fbc441e67804e67fad95a2e47321d2b4
Allan Kiplagat <allan.kiplagat@duke.edu>
2014-10-21 00:12:01 -0400

    refactored turtle screen edge wrapping

--- src/gui/factories/LineFactory.java
+++ src/gui/factories/LineFactory.java
@@ -2,10 +2,13 @@ package gui.factories;
 
 import gui.componentdrawers.ComponentInitializer;
 import gui.componentdrawers.TurtleScreenDrawer;
+import gui.turtlescreenwrap.Point2DPair;
+import gui.turtlescreenwrap.TurtleScreenWrapAround;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import com.sun.javafx.geom.Point2D;
 import javafx.scene.Node;
 import javafx.scene.shape.Line;
 
@@ -33,146 +36,24 @@ public class LineFactory extends ObjectFactory {
     }
 
     public List<Line> generateLines (Map<String, String> params) {
+        
+
+        Point2D origin = parseStringToPoints(params.get(ORIGIN));
+        Point2D dest = parseStringToPoints(params.get(DESTINATION));
+        //split into 'sub-lines'
+        List<Point2DPair> pointPairs = TurtleScreenWrapAround.
+                fragmentPoint2DPair(new Point2DPair(origin,dest));
+        
         List<Line> lineList = new ArrayList<Line>();
-
-        Line line = new Line();
-        double[] origin = parseStringToPoints(params.get(ORIGIN));
-        double[] destination = parseStringToPoints(params.get(DESTINATION));  
-
-        line.setStartX(TurtleScreenDrawer.GRID_WIDTH/2 + origin[0] );
-        line.setStartY(TurtleScreenDrawer.GRID_HEIGHT/2 - origin[1]);
-
-        if (destinationOffScreen(destination)) {
-
-            boolean vertical = (destination[0]==origin[0]);
-            double gradient = (destination[1]-origin[1])/(destination[0]-origin[0]);
-
-            double deltaX = 0.0;
-            double deltaY = 0.0;
-
-            boolean movingRight = true;
-            //if moving to right, set deltaX to distance from right edge
-            if (destination[0]>origin[0]) {      
-                deltaX= TurtleScreenDrawer.GRID_WIDTH/2-origin[0];
-            } else if  (destination[0]<origin[0]) {
-                deltaX = -(origin[0]-(-TurtleScreenDrawer.GRID_WIDTH/2));
-                movingRight = false;
-            }
-
-            boolean movingUp = true;
-            //if moving up, set deltaY to distance from top edge
-            if (destination[1]>origin[1]) {      
-                deltaY= TurtleScreenDrawer.GRID_HEIGHT/2-origin[1];
-            } else if (destination[1]<origin[1]) {
-                deltaY = -(origin[1]-(-TurtleScreenDrawer.GRID_HEIGHT/2));
-                movingUp = false;
-            }
-
-            //determine whether line exits screen through x edge or y edge            
-            boolean exitY = true;
-            double[] tempDestination;
-
-            if (!vertical) {
-                //if exit is through y edge, using deltaX will yield off-screen destination
-                tempDestination = new double[]{origin[0]+deltaX,origin[1]+gradient*deltaX};
-                exitY = destinationOffScreen(tempDestination);
-            } 
-
-            //determine new destination & the recursing origin-destination set
-            double[] recurseOrigin,recurseDestination;
-
-            //if moving vertically, use deltaY, keep x ordinate 
-            if (vertical) {
-                double newDestY = origin[1]+deltaY;
-
-                //exited top/bottom so start bottom/top
-                if (movingUp) {
-                    recurseOrigin = new double[]{origin[0],-TurtleScreenDrawer.GRID_HEIGHT/2};
-                    recurseDestination = new double[]{origin[0],
-                                                      -TurtleScreenDrawer.GRID_HEIGHT/2+((destination[1]-origin[1])-deltaY)};
-                } else {
-                    recurseOrigin = new double[]{origin[0],TurtleScreenDrawer.GRID_HEIGHT/2};
-                    recurseDestination = new double[]{origin[0],TurtleScreenDrawer.GRID_HEIGHT/2-
-                                                      ((origin[1]-destination[1])-(origin[1]-newDestY))};
-                }
-                destination[1] = newDestY;
-
-            } else {
-                //if exiting through y, use deltaY else use deltaX
-                if (exitY) {
-                    double newDestY = origin[1]+deltaY;
-                    double newDestX = origin[0]+deltaY/gradient;
-                    double remainderDeltaY = destination[1]-origin[1]-deltaY;
-
-                    //exited top/bottom so start bottom/top
-                    if (movingUp) {
-                        recurseOrigin = new double[]{newDestX,-TurtleScreenDrawer.GRID_HEIGHT/2};                     
-                        recurseDestination = new double[]{recurseOrigin[0]+remainderDeltaY/gradient,
-                                                          recurseOrigin[1]+remainderDeltaY};
-
-                    } else {
-                        recurseOrigin = new double[]{newDestX,TurtleScreenDrawer.GRID_HEIGHT/2};
-                        recurseDestination = new double[]{recurseOrigin[0]+remainderDeltaY/gradient,
-                                                          recurseOrigin[1]+remainderDeltaY};
-
-                    }
-
-                    destination[1] = newDestY;
-                    destination[0] = newDestX;
-
-                    //exiting through x so use delta x    
-                } else {
-                    double newDestY = origin[1]+deltaX*gradient;
-                    double newDestX = origin[0]+deltaX;
-                    double remainderDeltaX = destination[0]-origin[0]-deltaX;
-
-                    //exited left/right so start right/left
-                    if (movingRight) {
-                        recurseOrigin = new double[]{-TurtleScreenDrawer.GRID_WIDTH/2,newDestY};
-                        recurseDestination = new double[]{recurseOrigin[0]+remainderDeltaX,
-                                                          recurseOrigin[1]+remainderDeltaX*gradient};
-                    } else {
-                        recurseOrigin = new double[]{TurtleScreenDrawer.GRID_WIDTH/2,newDestY};
-                        recurseDestination = new double[]{recurseOrigin[0]+remainderDeltaX,
-                                                          recurseOrigin[1]+remainderDeltaX*gradient};
-                    }
-
-                    destination[1] = newDestY;
-                    destination[0] = newDestX;
-                }
-            }
-
-            line.setEndX(TurtleScreenDrawer.GRID_WIDTH/2 + destination[0] );
-            line.setEndY(TurtleScreenDrawer.GRID_HEIGHT/2 - destination[1]);
-            
-            //make recursive call
-            Map<String,String> recursiveParams = new HashMap<String,String>();
-            recursiveParams.put(ORIGIN,recurseOrigin[0]+" "+recurseOrigin[1]);
-            recursiveParams.put(DESTINATION, recurseDestination[0]+" "+recurseDestination[1]);
-            //add returned lines to list
-            List<Line> otherLines = this.generateLines(recursiveParams);
-            for (Line otherLine:otherLines) {
-                lineList.add(otherLine);
-            }         
-
-        } else {
-            line.setEndX(TurtleScreenDrawer.GRID_WIDTH/2 + destination[0] );
-            line.setEndY(TurtleScreenDrawer.GRID_HEIGHT/2 - destination[1]);
+        for (Point2DPair pointPair:pointPairs) {
+            Line line = new Line();
+            line.setStartX(pointPair.origin.x);
+            line.setStartY(pointPair.origin.y);
+            line.setEndX(pointPair.dest.x);
+            line.setEndY(pointPair.dest.y);
+            lineList.add(line);
         }
-        //destination = GridEdgeRules.applyRules(origin, destination);
-        //add current list to lineList
-        lineList.add(line);
         return lineList;
-    }
-
-    private boolean destinationOffScreen(double[] destination) {
-        boolean isOffScreen = (
-                destination[0]>TurtleScreenDrawer.GRID_WIDTH/2  ||
-                destination[0]<-TurtleScreenDrawer.GRID_WIDTH/2 ||
-                destination[1]>TurtleScreenDrawer.GRID_HEIGHT/2 ||
-                destination[1]<-TurtleScreenDrawer.GRID_HEIGHT/2
-                );
-        return isOffScreen;
     }
 
     /**
@@ -182,11 +63,11 @@ public class LineFactory extends ObjectFactory {
      * @param point
      * @return
      */
-    private double[] parseStringToPoints(String point) {
+    private Point2D parseStringToPoints(String point) {
         String[] splitPoint = point.split(" ");
 
-        double[] parsedPoint = new double[]{Double.parseDouble(splitPoint[0]),
-                                            Double.parseDouble(splitPoint[1])};
+        Point2D parsedPoint = new Point2D(Float.parseFloat(splitPoint[0]),
+                                           Float.parseFloat(splitPoint[1]));                                          
         return parsedPoint;
     }
 
---
+++ src/gui/turtlescreenwrap/VerticalTurtleScreenWrapAround.java
@@ -0,0 +1,6 @@
+package gui.turtlescreenwrap;
+
+import java.util.List;
+
+public class VerticalTurtleScreenWrapAround extends TurtleScreenWrapAround {
+}
