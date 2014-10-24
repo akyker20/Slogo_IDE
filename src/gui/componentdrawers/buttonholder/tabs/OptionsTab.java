4461d1e32f26a8fe8ac54666294817105f798d5e
Austin Kyker <amk66@duke.edu>
2014-10-23 23:23:01 -0400

    continued tab pane styling progress

--- src/gui/componentdrawers/buttonholder/tabs/OptionsTab.java
+++ src/gui/componentdrawers/buttonholder/tabs/OptionsTab.java
@@ -1,12 +1,14 @@
 package gui.componentdrawers.buttonholder.tabs;
 
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.VBox;
 
 public class OptionsTab extends Tab {
     public OptionsTab(Node[] features, String tabName){
-        VBox featureContainer = new VBox();
+        VBox featureContainer = new VBox(5);
+        featureContainer.setPadding(new Insets(5, 0, 5, 5));
         for(Node feature:features){
             featureContainer.getChildren().add(feature);
         }
