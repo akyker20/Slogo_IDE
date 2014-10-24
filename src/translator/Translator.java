c097b39e70aa33a190bfdba8cb619dcc3566c8f5
Steve Kuznetsov <steve.kuznetsov@duke.edu>
2014-10-24 19:22:46 -0400

    moved gui resources to reasonable place

--- src/translator/Translator.java
+++ src/translator/Translator.java
@@ -1,10 +1,7 @@
 package translator;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
