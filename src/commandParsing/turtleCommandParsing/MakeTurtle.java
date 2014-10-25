f47dfa05c768b1a58d50b3c51447869b58fea609
Stanley Yuan <stanley.yuan25@gmail.com>
2014-10-24 21:23:53 -0400

    Minor edit

--- src/commandParsing/turtleCommandParsing/MakeTurtle.java
+++ src/commandParsing/turtleCommandParsing/MakeTurtle.java
@@ -28,67 +28,6 @@ public class MakeTurtle extends CommandParser implements TurtleGenerator {
 		return 0;
 	}
 	
-	/*
-	 * 
-	 * 
-	 * 
-	 * 
-	 * @Override
-	protected double operateOnComponents(List<Double> components, Queue<DrawableObject> objectQueue)
-			throws RunTimeDivideByZeroException {
-		double distance = expressionComponents.get(0);
 
-		for (Turtle t : workspace.turtles.getActiveTurtles()) {
-			Location initialLocation = new Location(t.getLocation());
-			t.move(distanceToMove(distance));
-			Location finalLocation = t.getLocation();
-			if (t.pen.isPenDown()) {
-				objectQueue.add(generateDrawableObjectRepresentingLine(initialLocation, finalLocation));
-			}
-			objectQueue.add(generateDrawableObjectRepresentingTurtle(t));
-		}
-		return distance;
-	}
-	
-	public PenUp(WorkspaceState someWorkspace) {
-		super(someWorkspace);
-	}
-
-	@Override
-	public double parse(Iterator<String> commandString, Queue<DrawableObject> objectQueue)
-			throws SLOGOException {
-		for (Turtle t : workspace.turtles.getActiveTurtles()) {
-			t.pen.togglePenUp();
-		}
-		return 0;
-	}
-	
-	@Override
-	protected double operateOnComponents(List<Double> components, Queue<DrawableObject> objectQueue)
-			throws RunTimeDivideByZeroException {
-		Location destination = getDestinationLocation(components);
-		Location turtleLocation = new Location(destination);
-
-		for (Turtle t : workspace.turtles.getActiveTurtles()) {
-			turtleLocation = new Location(t.getLocation());
-			t.setLocation(destination);
-			t.rotate(getDestinationHeading(workspace));
-			if (t.pen.isPenDown()) {
-				objectQueue.add(generateDrawableObjectRepresentingLine(turtleLocation, destination));
-			}
-			objectQueue.add(generateDrawableObjectRepresentingTurtle(t));
-		}
-		return distanceBetweenPoints(turtleLocation, destination);
-	}
-
-	protected abstract Location getDestinationLocation(List<Double> components);
-
-	protected abstract double getDestinationHeading(WorkspaceState workspace);
-
-	private double distanceBetweenPoints(Location firstPoint, Location secondPoint) {
-		return Math.sqrt(Math.pow(secondPoint.getX() - firstPoint.getX(), 2)
-				+ Math.pow(secondPoint.getY() - firstPoint.getY(), 2));
-	}
-	 */
 
 }
