ef3b7831dce5c5b82f005035bae6636b8ae92278
Stanley Yuan <stanley.yuan25@gmail.com>
2014-10-26 02:20:08 -0400

    Finished tests

--- src/commandParsing/structuralCommandParsing/AskWith.java
+++ src/commandParsing/structuralCommandParsing/AskWith.java
@@ -1,36 +1,56 @@
 package commandParsing.structuralCommandParsing;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.Queue;
 
 import workspaceState.Turtle;
 import workspaceState.WorkspaceState;
+
 import commandParsing.CommandParser;
 import commandParsing.exceptions.SLOGOException;
+
 import drawableobject.DrawableObject;
 
-public class AskWith extends Ask {
+public class AskWith extends MultipleTurtleCommand {
 
 	public AskWith(WorkspaceState someWorkspace) {
 		super(someWorkspace);
 	}
 
 	@Override
-	protected void determineTurtlesToActivate(Iterator<String> commands, Queue<DrawableObject> objectQueue)
+	public double parse(Iterator<String> commandStringIterator, Queue<DrawableObject> objectQueue)
 			throws SLOGOException {
-dfgsf g
-		for (Turtle t : workspace.turtles.getAllTurtles()) {
-			double value = 0;
-			while (commands.hasNext()) {
-				String stringOfInterest = commands.next();
-				CommandParser parser = (CommandParser) createParser(stringOfInterest, workspace);
-				value = parser.parse(commands, objectQueue);
-			}
-			if (value != 0) {
-				activeTurtleIDList.add(t.getID());
-			}
-		}
 
+		saveCurrentActiveTurtles();
+		workspace.turtles.clearActiveTurtles();
+		
+		extractCommandsBetweenBraces(commandStringIterator);
+		for(Turtle t : workspace.turtles.getAllTurtles()){
+			determineIfTurtleIsValidRecipient(t, enclosedCommands.iterator(), objectQueue);
+		}
+		makeAndActivateGivenTurtles(objectQueue);
+		
+		extractCommandsBetweenBraces(commandStringIterator);
+		parseCommandsBetweenBraces(enclosedCommands.iterator(), objectQueue);
+		
+		workspace.turtles.clearActiveTurtles();
+		restoreActiveTurtles();
+		
+		return returnValue;
+
+	}
+
+	private void determineIfTurtleIsValidRecipient(Turtle t, Iterator<String> commandIterator, Queue<DrawableObject> objectQueue) throws SLOGOException {
+		workspace.turtles.clearActiveTurtles();
+		workspace.turtles.activateTurtle(t.getID());
+		String stringOfInterest = commandIterator.next();
+		CommandParser parser = (CommandParser) createParser(stringOfInterest, workspace);
+		double value = parser.parse(commandIterator, objectQueue);
+		if(value!=0){
+			activeTurtleIDList.add(t.getID());
+		}
+		workspace.turtles.clearActiveTurtles();
 	}
 
 }
---
+++ src/tests/commandTests/multipleTurtleCommandTests/AskWithTests.java
@@ -0,0 +1,218 @@
+package tests.commandTests.multipleTurtleCommandTests;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import gui.factories.LineFactory;
+import gui.factories.turtlefactory.TurtleFactory;
+
+import org.junit.Test;
+
+import commandParsing.CommandParser;
+import commandParsing.exceptions.SLOGOException;
+import drawableobject.DrawableObject;
+import tests.commandTests.CommandTester;
+
+public class AskWithTests extends CommandTester {
+	
+	@Test
+	public void SimpleAskWithTests() throws SLOGOException {
+		resetTesterVariables();
+		setUpCommands("askwith [ 1 ] [ fd 50 ]");
+
+		double f = 0;
+
+		while (commands.hasNext()) {
+			CommandParser parser = createCommand();
+			f = parser.parse(commands, objectQueue);
+		}
+		assertTrue(f == 50);
+
+		DrawableObject line = objectQueue.poll();
+		assertTrue(line.getParent().equals(LineFactory.PARENT));
+		assertTrue(line.getType().equals(LineFactory.TYPE));
+		assertTrue(line.getParameters().get(LineFactory.ORIGIN).equals("0.0 0.0"));
+		assertTrue(line.getParameters().get(LineFactory.DESTINATION).equals("0.0 50.0"));
+
+		DrawableObject turtle = objectQueue.poll();
+		assertTrue(turtle.getParent().equals(TurtleFactory.PARENT));
+		assertTrue(turtle.getType().equals(TurtleFactory.TYPE));
+		assertTrue(turtle.getParameters().get(TurtleFactory.HEADING).equals("0.0"));
+		assertTrue(turtle.getParameters().get(TurtleFactory.LOCATION).equals("0.0 50.0"));
+		assertTrue(turtle.getParameters().get(TurtleFactory.TURTLE_ID).equals(Integer.toString(1)));
+
+		assertTrue(objectQueue.size() == 0);
+		
+		assertTrue(workspace.turtles.getActiveTurtles().contains(workspace.turtles.getTurtleWithID(1)));
+		assertTrue(workspace.turtles.getActiveTurtles().size() == 1);
+	}
+	
+	@Test
+	public void MultipleTurtleAskWithTests() throws SLOGOException {
+		resetTesterVariables();
+		setUpCommands("mk askwith [ 1 ] [ fd 50 ]");
+
+		double f = 0;
+
+		while (commands.hasNext()) {
+			CommandParser parser = createCommand();
+			f = parser.parse(commands, objectQueue);
+		}
+		assertTrue(f == 50);
+		
+		DrawableObject turtle = objectQueue.poll();
+		assertTrue(turtle.getParent().equals(TurtleFactory.PARENT));
+		assertTrue(turtle.getType().equals(TurtleFactory.TYPE));
+		assertTrue(turtle.getParameters().get(TurtleFactory.HEADING).equals("0.0"));
+		assertTrue(turtle.getParameters().get(TurtleFactory.LOCATION).equals("0.0 0.0"));
+		assertTrue(turtle.getParameters().get(TurtleFactory.TURTLE_ID).equals(Integer.toString(2)));
+
+		DrawableObject line = objectQueue.poll();
+		assertTrue(line.getParent().equals(LineFactory.PARENT));
+		assertTrue(line.getType().equals(LineFactory.TYPE));
+		assertTrue(line.getParameters().get(LineFactory.ORIGIN).equals("0.0 0.0"));
+		assertTrue(line.getParameters().get(LineFactory.DESTINATION).equals("0.0 50.0"));
+
+		turtle = objectQueue.poll();
+		assertTrue(turtle.getParent().equals(TurtleFactory.PARENT));
+		assertTrue(turtle.getType().equals(TurtleFactory.TYPE));
+		assertTrue(turtle.getParameters().get(TurtleFactory.HEADING).equals("0.0"));
+		assertTrue(turtle.getParameters().get(TurtleFactory.LOCATION).equals("0.0 50.0"));
+		assertTrue(turtle.getParameters().get(TurtleFactory.TURTLE_ID).equals(Integer.toString(1)));
+		
+		line = objectQueue.poll();
+		assertTrue(line.getParent().equals(LineFactory.PARENT));
+		assertTrue(line.getType().equals(LineFactory.TYPE));
+		assertTrue(line.getParameters().get(LineFactory.ORIGIN).equals("0.0 0.0"));
+		assertTrue(line.getParameters().get(LineFactory.DESTINATION).equals("0.0 50.0"));
+
+		turtle = objectQueue.poll();
+		assertTrue(turtle.getParent().equals(TurtleFactory.PARENT));
+		assertTrue(turtle.getType().equals(TurtleFactory.TYPE));
+		assertTrue(turtle.getParameters().get(TurtleFactory.HEADING).equals("0.0"));
+		assertTrue(turtle.getParameters().get(TurtleFactory.LOCATION).equals("0.0 50.0"));
+		assertTrue(turtle.getParameters().get(TurtleFactory.TURTLE_ID).equals(Integer.toString(2)));
+
+		assertTrue(objectQueue.size() == 0);
+		
+		assertTrue(workspace.turtles.getActiveTurtles().contains(workspace.turtles.getTurtleWithID(1)));
+		assertTrue(workspace.turtles.getActiveTurtles().contains(workspace.turtles.getTurtleWithID(2)));
+		assertTrue(workspace.turtles.getActiveTurtles().size() == 2);
+	}
+	
+	@Test
+	public void MultipleTurtlesWithConditionAskWithTests() throws SLOGOException {
+		resetTesterVariables();
+		setUpCommands("mk mk mk ask [ 2 3 4 ] [ pu ] askwith [ pendown? ] [ fd 50 ]");
+
+		double f = 0;
+
+		while (commands.hasNext()) {
+			CommandParser parser = createCommand();
+			f = parser.parse(commands, objectQueue);
+		}
+		assertTrue(f == 50);
+		System.out.println(objectQueue);
+		
+		DrawableObject turtle = objectQueue.poll();
+		assertTrue(turtle.getParent().equals(TurtleFactory.PARENT));
+		assertTrue(turtle.getType().equals(TurtleFactory.TYPE));
+		assertTrue(turtle.getParameters().get(TurtleFactory.HEADING).equals("0.0"));
+		assertTrue(turtle.getParameters().get(TurtleFactory.LOCATION).equals("0.0 0.0"));
+		assertTrue(turtle.getParameters().get(TurtleFactory.TURTLE_ID).equals(Integer.toString(2)));
+		
+		turtle = objectQueue.poll();
+		assertTrue(turtle.getParent().equals(TurtleFactory.PARENT));
+		assertTrue(turtle.getType().equals(TurtleFactory.TYPE));
+		assertTrue(turtle.getParameters().get(TurtleFactory.HEADING).equals("0.0"));
+		assertTrue(turtle.getParameters().get(TurtleFactory.LOCATION).equals("0.0 0.0"));
+		assertTrue(turtle.getParameters().get(TurtleFactory.TURTLE_ID).equals(Integer.toString(3)));
+		
+		turtle = objectQueue.poll();
+		assertTrue(turtle.getParent().equals(TurtleFactory.PARENT));
+		assertTrue(turtle.getType().equals(TurtleFactory.TYPE));
+		assertTrue(turtle.getParameters().get(TurtleFactory.HEADING).equals("0.0"));
+		assertTrue(turtle.getParameters().get(TurtleFactory.LOCATION).equals("0.0 0.0"));
+		assertTrue(turtle.getParameters().get(TurtleFactory.TURTLE_ID).equals(Integer.toString(4)));
+		
+		assertTrue(workspace.turtles.getTurtleWithID(1).pen.isPenDown());
+		assertFalse(workspace.turtles.getTurtleWithID(2).pen.isPenDown());
+		assertFalse(workspace.turtles.getTurtleWithID(3).pen.isPenDown());
+		assertFalse(workspace.turtles.getTurtleWithID(4).pen.isPenDown());
+		
+
+		DrawableObject line = objectQueue.poll();
+		assertTrue(line.getParent().equals(LineFactory.PARENT));
+		assertTrue(line.getType().equals(LineFactory.TYPE));
+		assertTrue(line.getParameters().get(LineFactory.ORIGIN).equals("0.0 0.0"));
+		assertTrue(line.getParameters().get(LineFactory.DESTINATION).equals("0.0 50.0"));
+
+		turtle = objectQueue.poll();
+		assertTrue(turtle.getParent().equals(TurtleFactory.PARENT));
+		assertTrue(turtle.getType().equals(TurtleFactory.TYPE));
+		assertTrue(turtle.getParameters().get(TurtleFactory.HEADING).equals("0.0"));
+		assertTrue(turtle.getParameters().get(TurtleFactory.LOCATION).equals("0.0 50.0"));
+		assertTrue(turtle.getParameters().get(TurtleFactory.TURTLE_ID).equals(Integer.toString(1)));
+		
+		assertTrue(objectQueue.size() == 0);
+		
+		assertTrue(workspace.turtles.getActiveTurtles().contains(workspace.turtles.getTurtleWithID(1)));
+		assertTrue(workspace.turtles.getActiveTurtles().contains(workspace.turtles.getTurtleWithID(2)));
+		assertTrue(workspace.turtles.getActiveTurtles().contains(workspace.turtles.getTurtleWithID(3)));
+		assertTrue(workspace.turtles.getActiveTurtles().contains(workspace.turtles.getTurtleWithID(4)));
+		assertTrue(workspace.turtles.getActiveTurtles().size() == 4);
+	}
+	
+//	@Test
+//	public void ComplicatedAskWithTests() throws SLOGOException {
+//		resetTesterVariables();
+//		setUpCommands("askwith [ 1 ] [ fd 50 ]");
+//
+//		double f = 0;
+//
+//		while (commands.hasNext()) {
+//			CommandParser parser = createCommand();
+//			f = parser.parse(commands, objectQueue);
+//		}
+//		assertTrue(f == 50);
+//		objectQueue.poll();
+//		objectQueue.poll();
+//		objectQueue.poll();
+//		objectQueue.poll();
+//
+//		DrawableObject line = objectQueue.poll();
+//		assertTrue(line.getParent().equals(LineFactory.PARENT));
+//		assertTrue(line.getType().equals(LineFactory.TYPE));
+//		assertTrue(line.getParameters().get(LineFactory.ORIGIN).equals("0.0 0.0"));
+//		assertTrue(line.getParameters().get(LineFactory.DESTINATION).equals("0.0 50.0"));
+//
+//		DrawableObject turtle = objectQueue.poll();
+//		assertTrue(turtle.getParent().equals(TurtleFactory.PARENT));
+//		assertTrue(turtle.getType().equals(TurtleFactory.TYPE));
+//		assertTrue(turtle.getParameters().get(TurtleFactory.HEADING).equals("0.0"));
+//		assertTrue(turtle.getParameters().get(TurtleFactory.LOCATION).equals("0.0 50.0"));
+//		assertTrue(turtle.getParameters().get(TurtleFactory.TURTLE_ID).equals(Integer.toString(1)));
+//
+//		line = objectQueue.poll();
+//		assertTrue(line.getParent().equals(LineFactory.PARENT));
+//		assertTrue(line.getType().equals(LineFactory.TYPE));
+//		assertTrue(line.getParameters().get(LineFactory.ORIGIN).equals("0.0 0.0"));
+//		assertTrue(line.getParameters().get(LineFactory.DESTINATION).equals("0.0 50.0"));
+//
+//		turtle = objectQueue.poll();
+//		assertTrue(turtle.getParent().equals(TurtleFactory.PARENT));
+//		assertTrue(turtle.getType().equals(TurtleFactory.TYPE));
+//		assertTrue(turtle.getParameters().get(TurtleFactory.HEADING).equals("0.0"));
+//		assertTrue(turtle.getParameters().get(TurtleFactory.LOCATION).equals("0.0 50.0"));
+//		assertTrue(turtle.getParameters().get(TurtleFactory.TURTLE_ID).equals(Integer.toString(2)));
+//
+//		assertTrue(objectQueue.size() == 0);
+//		
+//		assertTrue(workspace.turtles.getActiveTurtles().contains(workspace.turtles.getTurtleWithID(1)));
+//		assertTrue(workspace.turtles.getActiveTurtles().contains(workspace.turtles.getTurtleWithID(2)));
+//		assertTrue(workspace.turtles.getActiveTurtles().contains(workspace.turtles.getTurtleWithID(3)));
+//		assertTrue(workspace.turtles.getActiveTurtles().contains(workspace.turtles.getTurtleWithID(4)));
+//		assertTrue(workspace.turtles.getActiveTurtles().contains(workspace.turtles.getTurtleWithID(5)));
+//		assertTrue(workspace.turtles.getActiveTurtles().size() == 5);
+//	}
+
+}
