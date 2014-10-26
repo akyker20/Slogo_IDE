b0a9d3098eeee475d1bb0ec935f82513554ade84
Austin Kyker <amk66@duke.edu>
2014-10-26 03:39:20 -0400

    workin on user defined commands popup

---
+++ src/gui/factories/ImageIndexFactory.java
@@ -0,0 +1,5 @@
+package gui.factories;
+
+public class ImageIndexFactory {
+
+}
--- src/gui/nonbuttonfeatures/tableviews/UserDefinedCommandsFeature.java
+++ src/gui/nonbuttonfeatures/tableviews/UserDefinedCommandsFeature.java
@@ -1,11 +1,24 @@
 package gui.nonbuttonfeatures.tableviews;
 
 import gui.factories.userdefinedcommands.DisplayedUserCommand;
+import gui.nonbuttonfeatures.PreviousCommandsFeature;
 import java.util.List;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 /**
  * This class is a list view used to store previous commands. All commands that are entered
@@ -17,12 +30,14 @@ import javafx.scene.control.cell.PropertyValueFactory;
  */
 public class UserDefinedCommandsFeature extends TableView<DisplayedUserCommand> {
 
+    private static final String POPUP_WIDTH = null;
+    private static final String POPUP_HEIGHT = null;
     private ObservableList<DisplayedUserCommand> myCommandsList;
 
     public UserDefinedCommandsFeature(List<DisplayedUserCommand> userDefinedCommands){
         myCommandsList = (ObservableList<DisplayedUserCommand>) userDefinedCommands;
 
-        
+
         TableColumn<DisplayedUserCommand,String> commandName = new TableColumn<DisplayedUserCommand,String>("Name");
         commandName.setCellValueFactory(new PropertyValueFactory("myName"));
         commandName.prefWidthProperty().bind(this.widthProperty().divide(3));
@@ -33,7 +48,37 @@ public class UserDefinedCommandsFeature extends TableView<DisplayedUserCommand> 
         this.setItems(myCommandsList);
         this.setPrefHeight(168);
         this.setLayoutY(20);
+        this.setOnMouseClicked(event->showCommandContent(event));
     }
-   
-    
+
+    /**
+     * When the user double clicks on a previous command that command will be displayed in the command
+     * line so the user can quickly run it by hitting enter.
+     * @param event
+     */
+    private void showCommandContent (MouseEvent event) {
+        if(event.getButton().equals(MouseButton.PRIMARY)){
+            DisplayedUserCommand selectedCommand = this.getSelectionModel().getSelectedItem();
+            if(event.getClickCount() == 2 && selectedCommand != null){
+                Stage popup = new Stage();
+                popup.setX(event.getScreenX());
+                popup.setY(event.getScreenY());
+                BorderPane pane = new BorderPane();
+                Scene scene = new Scene(pane);
+                Label label = new Label(selectedCommand.getMyName());
+                label.setStyle("-fx-font-size: 20px; -fx-font-weight: bold");
+                pane.setTop(label);
+                VBox vbox = new VBox();
+                vbox.setPadding(new Insets(10));
+                Label contentLabel = new Label(selectedCommand.getMyContent());
+                vbox.getChildren().add(contentLabel);
+                vbox.setAlignment(Pos.CENTER);
+                pane.setCenter(vbox); 
+                popup.setScene(scene);
+                popup.show();
+            }
+        }
+    }
+
+
 }
