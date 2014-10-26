0421cb275d5b127f336775a928db21c1af2bcea1
Stanley Yuan <stanley.yuan25@gmail.com>
2014-10-25 21:51:05 -0400

    Finished multiple turtle commands

--- src/commandParsing/structuralCommandParsing/AskWith.java
+++ src/commandParsing/structuralCommandParsing/AskWith.java
@@ -3,22 +3,34 @@ package commandParsing.structuralCommandParsing;
 import java.util.Iterator;
 import java.util.Queue;
 
+import workspaceState.Turtle;
 import workspaceState.WorkspaceState;
+import commandParsing.CommandParser;
 import commandParsing.exceptions.SLOGOException;
 import drawableobject.DrawableObject;
 
-public class AskWith extends StructuralCommand {
+public class AskWith extends Ask {
 
 	public AskWith(WorkspaceState someWorkspace) {
 		super(someWorkspace);
 	}
 
 	@Override
-	public double parse(Iterator<String> commandStringIterator, Queue<DrawableObject> objectQueue)
+	protected void determineTurtlesToActivate(Iterator<String> commands, Queue<DrawableObject> objectQueue)
 			throws SLOGOException {
-		
 
-		return 0;
+		for (Turtle t : workspace.turtles.getAllTurtles()) {
+			double value = 0;
+			while (commands.hasNext()) {
+				String stringOfInterest = commands.next();
+				CommandParser parser = (CommandParser) createParser(stringOfInterest, workspace);
+				value = parser.parse(commands, objectQueue);
+			}
+			if (value != 0) {
+				activeTurtleIDList.add(t.getID());
+			}
+		}
+
 	}
 
 }
